3a41bc2250cb31de35f6ea07619a03d2c975f4bf
Joseph Gordon <joseph.gordon@latimes.com>
2020-02-20 19:41:12 +0000

    commit 4

--- models/order_ltv.sql
+++ models/order_ltv.sql
@@ -2,7 +2,12 @@
 {{ config(materialized='table') }}
 
 
+with stg_orders as(
+select * from {{ ref('stg_orders') }}),
+
 with orders as(
-select * from {{ ref('stg_orders') }})
+select * from {{ ref('orders') }})
 
 select * from orders
+left join stg_orders
+ on orders.order_id = stg_orders.order_id
